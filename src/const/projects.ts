a4a3606dd1b3afc1a36e1578debb1191eec92573
The-Krishna <140198749+The-Krishna@users.noreply.github.com>
2023-07-24 16:20:45 +0530

    Update projects.ts

--- src/const/projects.ts
+++ src/const/projects.ts
@@ -6,47 +6,38 @@ interface Project {
 
 export const projects: Project[] = [
   {
-    name: "Introduction to Dark web, anonymity and cryptocurrency",
-    link: "https://codered.eccouncil.org/certificate/529dd537-899b-47a6-89a5-8280c734f42b?logged=true",
+    name: "GitHub README Tech Stack",
+    link: "https://github.com/0l1v3rr/github-readme-tech-stack?ref=0l1v3rr.github.io",
     category: "fullstack",
   },
   {
-    name: "Cybrary: Challange: Password123",
-    link: "https://app.cybrary.it/courses/api/certificate/CC-a85d0830-05a0-4edf-80ed-18360633c2b0/view",
+    name: "CLI File Manager",
+    link: "https://github.com/0l1v3rr/cli-file-manager?ref=0l1v3rr.github.io",
     category: "other",
   },
   {
-    name: "Cyber Threat Hunting Training",
-    link: "https://issuer.gutenbergcerts.com/pdf/20230119-ACTI-kb00_df287269-4a36-4586-bb15-560006622874.pdf",
+    name: "Pathfinding Visualizer",
+    link: "https://github.com/0l1v3rr/pathfinding-visualizer?ref=0l1v3rr.github.io",
     category: "frontend",
   },
   {
-    name: "GitHub Workshop",
-    link: "https://certificate.givemycertificate.com/c/60b42ac7-8a76-46ad-8a80-c1a15bee0d27",
+    name: "Hangman Game",
+    link: "https://github.com/0l1v3rr/hangman-game?ref=0l1v3rr.github.io",
     category: "frontend",
   },
   {
-    name: "CVE Series: Apache Spark (CVE-2022-33891)",
-    link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/SAP%20USA/5L6aBHz9ZYCs4eibk_SAP%20USA_2sWw6feNxh3GkoYoS_1679416303649_completion_certificate.pdf",
+    name: "Portfolio Website",
+    link: "https://github.com/0l1v3rr/0l1v3rr.github.io?ref=0l1v3rr.github.io",
     category: "frontend",
   },
   {
-    name: "Cyber Security Engineering Virtual Internship Program",
+    name: "Port Scanner",
     link: "https://github.com/0l1v3rr/port-scanner?ref=0l1v3rr.github.io",
     category: "other",
   },
   {
-    name: "Cyber security virtual experience program",
-    link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/mastercard/vcKAB5yYAgvemepGQ_Mastercard_2sWw6feNxh3GkoYoS_1679468519698_completion_certificate.pdf",
+    name: "Subnet Calculator",
+    link: "https://github.com/0l1v3rr/subnet-calculator?ref=0l1v3rr.github.io",
     category: "other",
   },
-  {
-    name: "Cyber Security Global Virtual Internship",
-    link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/Clifford%20Chance/33CKX5eTKH3dXer7X_Clifford%20Chance_2sWw6feNxh3GkoYoS_1679837167191_completion_certificate.pdf",
-  },
-  {
-    name: "Shields Up: Cybersecurity Virtual Experience Program",
-    link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/AIG/2ZFnEGEDKTQMtEv9C_AIG_2sWw6feNxh3GkoYoS_1679984838744_completion_certificate.pdf",
-  }
-    
-    ];
+];
