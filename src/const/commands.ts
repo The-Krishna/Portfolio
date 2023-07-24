a88683b9fe47ff72e0837fdf19230c68f48b6767
The-Krishna <140198749+The-Krishna@users.noreply.github.com>
2023-07-24 16:46:29 +0530

    Update commands.ts

--- src/const/commands.ts
+++ src/const/commands.ts
@@ -79,25 +79,25 @@ My name is Mishri Soni and I am Graduated with Bachelor of Science (Information 
 Currently, My study of Cyber Security and Digital forensics is ongoing from Bascom Bridge educational institute Pvt.Ltd. I have also participated in Virtual Internship program of 'Cyber Secured India'  which will be completed this month.
 As I don't have professional or high quality experience, but I've basic knowledge of our field and if something is new to me I'll be able to learn that thing and would like to increase and explore my knowledge and ability to do something new and challenging.
 <br><br>
-Certifications:-
+Certifications:-<br>
 1.	Name: Introduction to Dark web, anonymity and cryptocurrency
-    Link: https://codered.eccouncil.org/certificate/529dd537-899b-47a6-89a5-8280c734f42b?logged=true
+    Link: https://codered.eccouncil.org/certificate/529dd537-899b-47a6-89a5-8280c734f42b?logged=true<br><br>
 2.	Name: Cybrary: Challenge: Password123
-    Link: https://app.cybrary.it/courses/api/certificate/CC-a85d0830-05a0-4edf-80ed-18360633c2b0/view
+    Link: https://app.cybrary.it/courses/api/certificate/CC-a85d0830-05a0-4edf-80ed-18360633c2b0/view<br><br>
 3.  Name: Cyber Threat Hunting Training
-    Link: https://issuer.gutenbergcerts.com/pdf/20230119-ACTI-kb00_df287269-4a36-4586-bb15-560006622874.pdf
+    Link: https://issuer.gutenbergcerts.com/pdf/20230119-ACTI-kb00_df287269-4a36-4586-bb15-560006622874.pdf<br><br>
 4.  Name: Github Workshop
-    Link: https://certificate.givemycertificate.com/c/60b42ac7-8a76-46ad-8a80-c1a15bee0d27
+    Link: https://certificate.givemycertificate.com/c/60b42ac7-8a76-46ad-8a80-c1a15bee0d27<br><br>
 5.  Name: CVE Series: Apache Spark (CVE-2022-33891)
-    Link: https://app.cybrary.it/courses/api/certificate/CC-d1b4c24f-ce79-4d73-b9c1-be07a0355db8/view
+    Link: https://app.cybrary.it/courses/api/certificate/CC-d1b4c24f-ce79-4d73-b9c1-be07a0355db8/view<br><br>
 6.  Name: Cyber Security Engineering Virtual Internship Program
-    Link: https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/SAP%20USA/5L6aBHz9ZYCs4eibk_SAP%20USA_2sWw6feNxh3GkoYoS_1679416303649_completion_certificate.pdf
+    Link: https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/SAP%20USA/5L6aBHz9ZYCs4eibk_SAP%20USA_2sWw6feNxh3GkoYoS_1679416303649_completion_certificate.pdf<br><br>
 7.  Name: Cyber security virtual experience program
-    Link: https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/mastercard/vcKAB5yYAgvemepGQ_Mastercard_2sWw6feNxh3GkoYoS_1679468519698_completion_certificate.pdf
+    Link: https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/mastercard/vcKAB5yYAgvemepGQ_Mastercard_2sWw6feNxh3GkoYoS_1679468519698_completion_certificate.pdf<br><br>
 8.  Name: Cyber Security Global Virtual Internship
-    Link: https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/Clifford%20Chance/33CKX5eTKH3dXer7X_Clifford%20Chance_2sWw6feNxh3GkoYoS_1679837167191_completion_certificate.pdf
+    Link: https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/Clifford%20Chance/33CKX5eTKH3dXer7X_Clifford%20Chance_2sWw6feNxh3GkoYoS_1679837167191_completion_certificate.pdf<br><br>
 9.  Name: Shields Up: Cyber security Virtual Experience Program
-    Link: https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/AIG/2ZFnEGEDKTQMtEv9C_AIG_2sWw6feNxh3GkoYoS_1679984838744_completion_certificate.pdf
+    Link: https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/AIG/2ZFnEGEDKTQMtEv9C_AIG_2sWw6feNxh3GkoYoS_1679984838744_completion_certificate.pdf<br><br>
 
 
 
